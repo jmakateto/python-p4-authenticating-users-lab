d47f9b6b8dfe3797ec29252105dd9f9c3e97c95b
john makateto <jmakateto@gmail.com>
2023-10-16 14:40:05 +0300

    made changes

--- server/app.py
+++ server/app.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Article, User
 
 app = Flask(__name__)
@@ -18,40 +15,28 @@ db.init_app(app)
 
 api = Api(app)
 
-class ClearSession(Resource):
+class Sessions(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        user = User.query.filter_by(username=username).first()
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {'message': 'Invalid username'}, 401
 
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
-
+        session.pop('user_id', None)
         return {}, 204
 
-class IndexArticle(Resource):
-    
     def get(self):
-        articles = [article.to_dict() for article in Article.query.all()]
-        return articles, 200
+        if 'user_id' in session:
+            user = User.query.get(session['user_id'])
+            if user:
+                return user.to_dict(), 200
+        return {}, 401
 
-class ShowArticle(Resource):
-
-    def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
-
-        if session['page_views'] <= 3:
-
-            article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
-
-            return make_response(article_json, 200)
-
-        return {'message': 'Maximum pageview limit reached'}, 401
-
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Sessions, '/login', '/logout', '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
